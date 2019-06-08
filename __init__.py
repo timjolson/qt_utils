f887d39b59e0536dd8de1730b93ef61fcb1baa8b
timjolson <timjolson@user.noreplay.github.com>
2019-06-08 05:07:09 -0500

    added submodules

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from qt_utils import colors, designer, widgets, verticallabelwidget
+from qt_utils import colors, designer, widgets, verticallabelwidget, adjustablewidget, entrywidget, sympyentrywidget
 from qt_utils import loggableQtName, eventExcludesButtons, eventIncludesButtons, eventMatchesButtons, getCurrentColor
