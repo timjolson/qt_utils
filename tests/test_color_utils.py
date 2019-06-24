ba92b1ef4058e4df28ab83937e472c1b2c6d8c1c
timjolson <tim.lsn@gmail.com>
2019-06-24 02:31:12 -0500

    Updated colorList items to namedtuples: ColorItem now has .names .hex .rgb

--- tests/test_color_utils.py
+++ tests/test_color_utils.py
@@ -5,15 +5,15 @@ def test_rgb_vs_hex():
     # check all colors in colorList
     for c in colorList:
         # make sure conversions work
-        assert '#'+rgb_to_hex(c[2]) == c[1]
-        assert hex_to_rgb(c[1]) == c[2]
+        assert '#'+rgb_to_hex(c.rgb) == c.hex
+        assert hex_to_rgb(c.hex) == c.rgb
 
 
 def test_findColor():
     # check all colors in colorList
     for C in colorList:
         # possible identifiers are all names in C[0], hex string in C[1], and rgb tuple in C[2]
-        clist = C[0] + list((C[1], C[2]))
+        clist = C.names + list((C.hex, C.rgb))
 
         # each identifier returns correct color
         for c in clist:
@@ -22,6 +22,6 @@ def test_findColor():
     # check all colors in colorList
     for c in colorList:
         # adjust rgb values slightly to test the search algorithm (which gets closest by rgb tuple_distance)
-        assert c == findColor((c[2][0]+2, c[2][1]-2, c[2][2]))
-        assert c == findColor((c[2][0], c[2][1]+2, c[2][2]-2))
-        assert c == findColor((c[2][0]-2, c[2][1], c[2][2]+2))
+        assert c == findColor((c.rgb.r+2, c.rgb.g-2, c.rgb.b))
+        assert c == findColor((c.rgb.r, c.rgb.g+2, c.rgb.b-2))
+        assert c == findColor((c.rgb.r-2, c.rgb.g, c.rgb.b+2))
