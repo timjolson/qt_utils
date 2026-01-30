bb83f82d497dbb8eec82e9913f7319988f8a45c4
timjolson <timjolson@users.noreply.github.com>
2026-01-30 11:46:03 -0600

    updated contact email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.6",
     description='PyQt5 helpers and widgets, qtDesigner helpers',
     author = 'timjolson',
-    author_email = 'timjolson@user.noreply.github.com',
+    author_email = '5532473+timjolson@users.noreply.github.com',
     packages=find_packages(),
     install_requires=['PyQt5==5.9'],
     tests_require=['pytest', 'pytest-qt'],
